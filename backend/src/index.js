8e063436643ed4fa7aef6c346ef61784678f0fe7
Murilo Gomes <murilo.goomes@hotmail.com>
2020-03-25 16:35:15 -0300

    Front-End da Aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
+const cors = require('cors');
 const express = require('express');
 const routes = require('./routes');
-const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
+
 
 app.listen(3333);
